338d63ed931d9614e554f3c91facbe86e8450e86
Michael Byrd <23690719+thealchemist2016@users.noreply.github.com>
2019-11-05 09:23:02 -0600

    srber

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-('.navbar').on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+$(".navbar").on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
 });
 
-const Honeybadger = require("path/to/honeybadger");
-Honeybadger.configure({
-  apiKey: '039edf52'
+import { configure } from "path/to/honeybadger";
+configure({
+  apiKey: "039edf52"
 });
