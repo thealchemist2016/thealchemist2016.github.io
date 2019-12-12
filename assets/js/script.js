3676abf136ea568acf0fe143970804a3e1bbd875
Michael Byrd <michaelbyrd7741@gmail.com>
2019-12-12 06:52:25 -0600

    social icoon links

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,7 @@ import { configure } from "path/to/honeybadger";
 configure({
   apiKey: "039edf52"
 });
+
+function externalLinks() { for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) { var b = c[a]; b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } } ; externalLinks();
+
+Read more: https://html.com/attributes/a-target/#s
