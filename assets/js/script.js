138e583951e92f5d00a0820d1138d6a92573bc7c
Michael Byrd <23690719+thealchemist2016@users.noreply.github.com>
2020-01-03 17:18:27 -0600

    fixed footer icon radius

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,4 @@ configure({
 
 function externalLinks() { for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) { var b = c[a]; b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } } ; externalLinks();
 
-Read more: https://html.com/attributes/a-target/#s
+Read //html.com/attributes/a-target/#s
