fe0841663f68eb4d724078cec9a08e6a55e642fb
Jessica Green <28920030+PhoenixTechJG@users.noreply.github.com>
2020-01-20 03:07:06 -0600

    toolset logos added and tribute page changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-$(".navbar").on("click", 'a[href^="#"]', function(event) {
-  event.preventDefault();
 
-  $("html, body").animate(
-    {
-      scrollTop: $($.attr(this, "href")).offset().top
-    },
-    500
-  );
-});
+$('.hamburger').on('click', function () {
+
+  $('.menu').toggleClass('open');
+  
+  });
+  
+  $( '.menu a' ).on("click", function(){
+  $('.menu').hide();
+  });
 
 import { configure } from "path/to/honeybadger";
 configure({
   apiKey: "039edf52"
 });
+
+function externalLinks() { for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) { var b = c[a]; b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } } ; externalLinks();
+
+Read //html.com/attributes/a-target/#s
