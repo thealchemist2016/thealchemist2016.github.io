cc58f6835bcd3fab83197cde5ece553f255296fa
Michael James Byrd Jr <michaelbyrd7741@gmail.com>
2026-06-06 09:21:14 -0500

    feat: redirect build to docs and remove github action due to account billing lock

---
+++ docs/XS-Records/precache-manifest.8138c4d9e0a256af4bd6ca9854b00268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4039671a37b5ac89bee34f3cd3f1b65",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64f66362813cd0ccd784",
+    "url": "./static/css/main.b2886593.chunk.css"
+  },
+  {
+    "revision": "76d5299a7e82e99d865e",
+    "url": "./static/js/2.24a5a369.chunk.js"
+  },
+  {
+    "revision": "64f66362813cd0ccd784",
+    "url": "./static/js/main.4be3c0c2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0e566bf4f228ac4a8fabdd0af8697e91",
+    "url": "./static/media/mixer-sm-fixed.0e566bf4.jpg"
+  }
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/users': 'http://localhost:3001',
